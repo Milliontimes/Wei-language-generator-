251b09d9fd2f81f959464b0d6ad8db3843064fb3
qq3027877467 <88929566+qq3027877467@users.noreply.github.com>
2021-08-14 18:46:44 +0800

    How to talk nonsense 如何在威语上比威语缔造者更胜一筹.py
    This is the first version, maybe it will be updated.第一个版本，可能会被更新。

---
+++ How to talk nonsense 如何在威语上比威语缔造者更胜一筹.py
@@ -0,0 +1,2 @@
+shuchu = '{shabi}{feihua}了，但是没有完全{feihua}，这是为什么呢？因为{shabi}没有完全{feihua}，我一般称这种情况为“{feihua}了，没有完全{feihua}”这种问题，值得{shabi}深思，就像泡方便面其实不能放牙膏，拖把是用来拖地的，玩游戏时一定得玩儿才能玩儿。打这么多字，其实就想让{shabi}明白{shabi}应该明白，明白的道理，就像其实{shabi}{feihua}了，但没有完全{feihua}'
+shuchu.format(feihua= '废话', shabi= '煞笔')
